84d2ce45768285ab9e395313ef0c3132b6e2d9b0
Wrubleski <henrike.hip@live.com>
2020-02-12 17:08:14 -0300

    finalizado deepComparison

--- chapter-four/deepComparison.js
+++ chapter-four/deepComparison.js
@@ -3,21 +3,23 @@ The == operator compares objects by identity. But sometimes, you'd prefer to com
 
 Write a function deepEqual that takes two values and returns true only if they are the same value or are objects with the same properties, where the values of properties are equal when compared with recursive call to deepEqual.
 
-to find out whether values should be compared directly (use the === operator for that) or have their properties compared, you can use the typeof operator. If it produces "object" for both values, you should do a deep comparison. But you have to take one silly exception into account: because of a historical accident, typeof null also produces "object".
+To find out whether values should be compared directly (use the === operator for that) or have their properties compared, you can use the typeof operator. If it produces "object" for both values, you should do a deep comparison. But you have to take one silly exception into account: because of a historical accident, typeof null also produces "object".
 
 The Object.keys function will be useful when you need to go over the properties of objects to compare them.
 **/
 
 function deepEqual(obj1, obj2){
-	if (typeof(obj1) === 'object' && typeof(obj2) === 'object'){
-		obj1Keys = Object.keys(obj1);
-		obj2Keys = Object.keys(obj2);
-		
-
-
+	if (typeof(obj1) === 'object' && typeof(obj2) === 'object' && obj1 !== null && obj2 !== null){
+		let obj1Keys = Object.keys(obj1);
+		let obj2Keys = Object.keys(obj2);
+		if (obj1Keys.length !== obj2Keys.length) return false;
+		for (i = 0; i < obj1Keys.length; i++){
+			if (obj1Keys[i] !== obj2Keys[i]) return false;
+			return deepEqual(obj1[obj1Keys[i]], obj2[obj2Keys[i]]);
+		}
 	} else {
 		return obj1 === obj2;
-	}
+	}	
 }
 
 
