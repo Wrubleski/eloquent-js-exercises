68f30913fe88370221930a4b4ab56dbcc53afe68
wrubleski <henrike.hip@live.com>
2020-02-18 14:47:55 -0300

    concluido os ex 1 e 2 do ch 5, e iniciado o ex 3 do ch 5

--- sandbox/script.js
+++ sandbox/script.js
@@ -92,6 +92,8 @@ console.log(reduce([1,2,3,4,5], (a,b) => a + b, 0))*/
 
 /*console.log([1,2,3,4].reduce((a,b) => a+b));*/
 
+
+
 /*function characterCount(script) {
 	return script.ranges.reduce((count, [from, to]) => {
 		return count + (to - from);
@@ -129,10 +131,10 @@ console.log(Math.round(average(
 }
 */
 
-function charaterScript(code){
+function characterScript(code){
 	for (let script of SCRIPTS){
 		if (script.ranges.some(([from, to]) => {
-			return code >= from && < to;
+			return code >= from && code < to;
 		})) {
 			return script;
 		}
@@ -141,6 +143,34 @@ function charaterScript(code){
 }
 
 
-console.log(characterScript(121));
+
+function countBy(items, groupName) {
+	let counts = [];
+	for (let item of items) {
+		let name = groupName(item);
+		let know = counts.findIndex(c => c.name == name);
+		if (know == -1) {
+			counts.push({name, count: 1});
+		} else {
+			counts[know].count++;
+		}
+	}
+	return counts;
+}
 
 
+function textScripts(text) {
+	let scripts = countBy(text, char => {
+		let script = characterScript(char.codePointAt(0));
+		return script ? script.name : "none";
+	}).filter(({name}) => name != "none");
+
+	let total = scripts.reduce((n, {count}) => n + count, 0);
+	if (total == 0) return "No scripts found";
+
+	return scripts.map(({name, count}) => {
+		return `${Math.round(count * 100 / total)}% ${name}`;
+	}).join(",");
+}
+
+console.log(textScripts('英国的狗说"woof", 俄罗斯的狗说"тяв"'));
