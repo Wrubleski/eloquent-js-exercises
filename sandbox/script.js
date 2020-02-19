ab50ba4dcc5803a1ea8f077df05b8dd8ea561cc8
Wrubleski <henrike.hip@live.com>
2020-02-19 14:51:13 -0300

    testes na sandbox

--- sandbox/script.js
+++ sandbox/script.js
@@ -129,10 +129,10 @@ console.log(Math.round(average(
 }
 */
 
-function charaterScript(code){
+function characterScript(code){
 	for (let script of SCRIPTS){
 		if (script.ranges.some(([from, to]) => {
-			return code >= from && < to;
+			return code >= from && code < to;
 		})) {
 			return script;
 		}
